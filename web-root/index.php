98d00cff6b93d6c9856a09db0cffa90b6284e58f
Greg Lindahl <lindahl@pbm.com>
2020-02-23 11:59:33 -0800

    update about

--- web-root/index.php
+++ web-root/index.php
@@ -28,7 +28,7 @@ header('Content-Type: text/html; charset=utf-8');
     <!-- analytics disabled for host "<?= $_SERVER['HTTP_HOST'] ?>" -->
    <?php } ?>
     
-    <title>(modified) Stuff in Space</title>
+    <title>Stuff in Space</title>
     
   </head>
   <body>
@@ -135,21 +135,19 @@ header('Content-Type: text/html; charset=utf-8');
       <div id="menu-about" class="menu-item">
         <div class="menu-title">About</div>
         <div id="about-box" class="menubox submenu">
-          <span class="box-header">(modified) Stuff in Space</span>
+          <span class="box-header">Stuff in Space</span>
 	  <p>constellation.fu.gg is a modified version of <a href="http://stuffin.space">Stuff in Space</a>.</p>
 
           <p>Stuff in Space is a realtime 3D map of objects in Earth orbit, visualized using WebGL.</p>
           
-          <p>The website updates daily with orbit data from <a href="http://www.space-track.org">Space-Track.org</a> 
-          and uses the excellent <a href="https://github.com/shashwatak/satellite-js">satellite.js</a> Javascript library
-          to calculate satellite positions.</p>
-          
-          <span class="box-header">About the author</span>
-        <!--  <p>My name is James Yoder; I'm an alumnus of <a href="http://www.usfirst.org/roboticsprograms/frc"><i>FIRST</i> Robotics
-          Competition </a> (FRC) <a href="http://team624.org">Team 624</a> and an incoming Electrical and Computer Engineering freshman at the 
-          University of Texas at Austin. </p> -->
-          <p>Contact: <a href="mailto:info@stuffin.space">info@stuffin.space</a></p>
-          <p>See Stuff in Space on <a href="https://github.com/jeyoder/ThingsInSpace"> GitHub </a></p>
+          <span class="box-header">Credits</span>
+<p>Portions Copyright (c) 2020 by Greg Lindahl</p>
+
+<p><a href="http;//stuffin.space">StuffInSpace</a> (<a href="https://github.com/jeyoder/StuffInSpace/">github repo</a>) by James Yoder</p>
+
+<p>satellite.js (c) 2013 by Sashwat Kandadai and UCSC</p>
+
+<p>gl-matrix (c) 2013, Brandon Jones, Colin MacKenzie IV</p>
         </div>
       </div>
     </div>
